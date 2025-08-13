849320f6b5dbde51011ee112bdd7bd85508b6636
ColeWill <wcole2222@gmail.com>
2025-08-12 21:37:56 -0600

    updated eslint config, removed eslint ignore, removed unused react imports that were causing the build to fail

--- eslint.config.js
+++ eslint.config.js
@@ -3,24 +3,43 @@ import storybook from "eslint-plugin-storybook";
 
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  {
+    ignores: ['dist', 'node_modules', 'build', '.eslintrc.cjs'],
+  },
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      react.configs.flat.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
     },
   },
-], storybook.configs["flat/recommended"]);
+  ...storybook.configs["flat/recommended"]
+]);
